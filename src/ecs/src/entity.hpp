a6497851bca5685c696960ba0c8d071aa6f6e9db
LostAFSendHelp <49492620+LostAFSendHelp@users.noreply.github.com>
2020-10-20 21:32:16 +0700

    Added some doxygen for entity

--- src/ecs/src/entity.hpp
+++ src/ecs/src/entity.hpp
@@ -9,21 +9,101 @@ namespace laf {
     public:
         const uint32_t id_;
 
+
+
+        /**
+         * @brief Constructs a new entity object. This is a mere container for components.
+         */
         entity();
+
+
+
+        /**
+         * @brief Destroys the entity object.
+         */
         ~entity();
 
+        
+        
+        /**
+         * @brief Attaches a component to the current entity. Detaches it from its owning entity, if any.
+         * 
+         * @param target The component to be attached. 
+         * @return true if attachment is successful.
+         * @return false otherwise.
+         */
         bool attach_component(const std::shared_ptr<component>& target);
+
+
+
+        /**
+         * @brief Removes a child component with the same id as that of the target, if any.
+         * 
+         * @param target The component to be removed by id.
+         * @return true if removal is successful.
+         * @return false otherwise.
+         */
         bool detach_component(const std::shared_ptr<component>& target);
+
+
+
+        /**
+         * @brief Removes a child component by id, if any.
+         * 
+         * @param id The id of the component that is to be removed.
+         * @return true if removal is successful.
+         * @return false otherwise.
+         */
         bool detach_component(uint32_t id);
+
+
+
+        /**
+         * @brief Removes all child components
+         */
         void flush_components();
 
+
+
+        /**
+         * @brief Gets the component object of the specified type.
+         * 
+         * @tparam T The component type to be retrieved, must be component-derived.
+         * @return std::shared_ptr<T> 
+         */
         template<typename T>
         std::shared_ptr<T> get_component() const;
 
+
+
+        /**
+         * @brief Calls awake() of all child components.
+         */
         void awake();
+
+
+
+        /**
+         * @brief Calls update() of all child components.
+         */
         void update();
+
+
+
+        /**
+         * @brief Adds a mesh to the current entity.
+         * 
+         * @param mesh The mesh to be added.
+         */
         void add_mesh(const std::shared_ptr<mesh>& mesh);
 
+
+
+        /**
+         * @brief Gets the mesh of the current entity, if any.
+         * 
+         * @return std::shared_ptr<mesh> 
+         */
         inline std::shared_ptr<mesh> mesh() const {
             return mesh_;
         }
