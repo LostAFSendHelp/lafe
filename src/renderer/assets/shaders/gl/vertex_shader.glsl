1deba8eb8ab4e36d8972ca4756bdfe93d82397f2
LostAFSendHelp <49492620+LostAFSendHelp@users.noreply.github.com>
2020-10-21 21:51:13 +0700

    Switched to calculating lighting in view space

--- src/renderer/assets/shaders/gl/vertex_shader.glsl
+++ src/renderer/assets/shaders/gl/vertex_shader.glsl
@@ -6,14 +6,19 @@ layout (location = 1) in vec3 in_normal;
 uniform mat4 u_model;
 uniform mat4 u_projection;
 uniform mat4 u_view;
+uniform vec4 u_light_source_position = vec4(.0f, .0f, .0f, 1.0f);
 
-out vec3 v_color;
 out vec3 v_normal;
 out vec3 v_fragment_position;
+out vec3 v_light_source_position;
 
 void main() {
+    // position
     vec4 v_position = u_model * vec4(in_position, 1.0f);
     gl_Position = u_projection * u_view * v_position;
-    v_fragment_position = vec3(v_position);
-    v_normal = vec3(u_model * vec4(in_normal, 0.0f));
+
+    // vectors in view space
+    v_fragment_position = vec3(u_view * v_position);
+    v_normal = vec3(u_view * u_model * vec4(in_normal, 0.0f));
+    v_light_source_position = vec3(u_view * u_light_source_position);
 }
